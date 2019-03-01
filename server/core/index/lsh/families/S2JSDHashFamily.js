5cd754d68d1196b7392c2ae2d5d35a6892c1b54e
XaeroSkills <xaero.skills@yahoo.com>
2019-03-02 00:19:24 +0300

    delete

--- server/core/index/lsh/families/S2JSDHashFamily.js
+++
@@ -1,1 +0,0 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0});const HashFamily_1=require("../HashFamily"),S2JSDHash_1=require("./S2JSDHash");class S2JSDHashFamily extends HashFamily_1.default{constructor(s,e){super(),this.dimensions=s,this.w=e}createHashFunction(){return new S2JSDHash_1.default(this.dimensions,this.w)}}exports.default=S2JSDHashFamily;
